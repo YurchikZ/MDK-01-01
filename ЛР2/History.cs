930f4876e8854d9c4332c8816b5851afa5032b94
Никитос <dbdhdfzvek4u2vhs@gmail.com>
2024-11-12 09:29:12 +0300

    Добавьте файлы проекта.

---
+++ ЛР2/History.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace жЕСТЬ
+{
+    public struct Sales
+    {
+        public int quantity;
+        public int datе;
+        public mebel product;
+    }
+    internal class History
+
+    {
+        // У нас было пять пакетиков травы, семьдесят пять ампул мескалина, пять пакетиков диэтиламид лизергиновой кислоты или ЛСД, солонка наполовину наполненная кокаином и целое море разноцветных амфетаминов, барбитуратов и транквилизаторов........
+    }
+}
---
+++ ЛР2/mebel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace жЕСТЬ
+{
+    public class mebel
+    {
+        string mebelname;
+        double mebelvalue;
+
+        public void getnames(string n)
+        {
+            mebelname = n;
+        }
+        public string getvaluestring()
+        {
+            return mebelname;
+        }
+        public void Setvalue(double v)
+        {
+            v = mebelvalue;
+        }
+        public mebel(string NAMESS, double PRICEE)
+        {
+            mebelname = NAMESS;
+            mebelvalue = PRICEE;
+        }
+    }
+}
